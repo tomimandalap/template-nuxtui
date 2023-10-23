f77b4c26b00c7349c26b16355e8c56e0b193e873
tomimandalap <tommymandalaputra@gmail.com>
2023-10-23 15:42:13 +0700

    fix: add responsive content datepicker

--- pages/admin/documentation/datepicker.vue
+++ pages/admin/documentation/datepicker.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'dashboard'
+  layout: 'dashboard',
 })
 
 const ui = {
   body: {
-    padding: 'p-3.5 flex flex-col justify-center items-start'
+    padding: 'p-3.5 flex flex-col justify-center items-start',
   },
 }
 </script>
@@ -14,21 +14,21 @@ const ui = {
   <h1 class="text-xl font-semibold">Datepicker</h1>
 
   <div class="grid grid-cols-12 gap-5 my-3.5">
-    <div class="col-span-12 md:col-span-3">
+    <div class="col-span-12 lg:col-span-4">
       <UCard :ui="ui">
         <h4 class="text-base mb-2">Button</h4>
         <calendar-button />
       </UCard>
     </div>
 
-    <div class="col-span-12 md:col-span-3">
+    <div class="col-span-12 lg:col-span-4">
       <UCard :ui="ui">
         <h4 class="text-base mb-2">Input</h4>
         <calendar-input />
       </UCard>
     </div>
 
-    <div class="col-span-12 md:col-span-3">
+    <div class="col-span-12 lg:col-span-4">
       <UCard :ui="ui">
         <h4 class="text-base mb-2">Input Range</h4>
         <calendar-input-range />
