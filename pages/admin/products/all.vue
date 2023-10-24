997ca99849fbe5d17d99cd3d763d04d9798ccf40
tomimandalap <tommymandalaputra@gmail.com>
2023-10-24 15:15:37 +0700

    feat: implementation infinity scroll

--- pages/admin/products/all.vue
+++ pages/admin/products/all.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { IUser } from '@/types/responses/products_response_type'
 definePageMeta({
   layout: 'dashboard',
 })
@@ -9,15 +10,43 @@ const params = ref<{ q: string; skip: number; limit: number }>({
   limit: 10,
 })
 
-const { getData } = useProducts()
-const { data, pending } = getData(params)
+const products = ref<IUser[]>([])
+const total = ref<number>(0)
 
-const products = computed(() => data.value?.products || [])
+const { getData } = useProducts()
+const load = async (): Promise<void> => {
+  const { data } = await getData(params)
+
+  if (data.value && data.value.products) {
+    products.value = [...products.value, ...data.value.products]
+    total.value = data.value?.total || 0
+  }
+}
+
+// first load
+await load()
+
+const productContainer = ref<HTMLElement | null>(null)
+
+useInfiniteScroll(
+  productContainer,
+  async () => {
+    params.value.skip += 10
+
+    if (params.value.skip < total.value) await load()
+
+    return
+  },
+  { distance: 100 },
+)
 </script>
 <template>
   <h1 class="text-xl font-semibold mb-5">Products</h1>
 
-  <div class="grid grid-cols-12 gap-5">
+  <div
+    ref="productContainer"
+    class="grid grid-cols-12 gap-5 h-[calc(100vh-14rem)] overflow-auto"
+  >
     <div
       v-for="(product, index) in products"
       :key="`product-card-${index}`"
