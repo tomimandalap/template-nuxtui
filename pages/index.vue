2afc6509bfe5fe925a30ad8ccac46e361e6d16c4
tomimandalap <tommymandalaputra@gmail.com>
2023-10-24 16:31:02 +0700

    fix: change target link dashboard in landing

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,24 @@
         <div class="flex justify-between">
           <h1>Welcome to Nuxt UI Starter</h1>
           <ColorScheme>
-            <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" />
+            <USelect
+              v-model="$colorMode.preference"
+              :options="['system', 'light', 'dark']"
+            />
           </ColorScheme>
         </div>
       </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank" class="me-3">Open Nuxt UI
-        Documentation</UButton>
-      <UButton icon="i-iconoir-pc-check" variant="outline" to="/admin">Dashboard</UButton>
+      <UButton
+        icon="i-heroicons-book-open"
+        to="https://ui.nuxt.com"
+        target="_blank"
+        class="me-3"
+      >
+        Open Nuxt UI Documentation
+      </UButton>
+      <UButton icon="i-iconoir-pc-check" variant="outline" to="/admin/incomes">
+        Dashboard
+      </UButton>
     </UCard>
   </UContainer>
 </template>
